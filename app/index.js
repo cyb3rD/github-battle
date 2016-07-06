c5b61adce3e16d3f25ca212c006aaa03ea14eb7f
Nikolay Ignatyev <noilgt@gmail.com>
2016-07-06 16:51:30 +0300

    Added Avatar component, consusting of 3 nested components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,53 @@
+var USER_DATA = {
+  name: 'Nikolay Ignatyev',
+  username: 'cyb3D',
+  image: 'https://avatars3.githubusercontent.com/u/2452690?v=3&s=460'
+}
+
 var React =  require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+  render: function() {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function() {
     return (
-      <div> Hello World! </div>
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+      return (
+        <div>
+          <p>{this.props.name}</p>
+        </div>
+      )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA}/>,
   document.getElementById('app')
 );
